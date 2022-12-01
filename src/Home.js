82dacaa361e09764341bbb361e61d23bd1057749
Nawam Sahasra Mandara Denawakage <nawamsahasra27th@gmail.com>
2022-12-01 12:58:05 +0530

    adding files

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log("hello, guys");
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("Hello " + name);
+    }
     return (
         <div className="hpme">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() =>{
+                handleClickAgain("Mario")
+            }}>Click me Again</button>
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -5,9 +5,7 @@ const Navbar = () => {
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create" style={{
-                    color: "white",
-                    backgroundColor:"#f1356d",
-                    borderRadius: "8px"
+                    
                 }}>New Blog</a>
             </div>
         </nav>
